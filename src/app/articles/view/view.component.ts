04c8fdb36b7b22510e2131318bc2280bd389c872
Miri Engel <miri.engel10@gmail.com>
2017-10-20 10:24:45 +0300

    list articles

--- src/app/articles/view/view.component.ts
+++ src/app/articles/view/view.component.ts
@@ -4,9 +4,6 @@ import { AuthenticationService } from '../../authentication/authentication.servi
 import { ArticlesService } from '../articles.service';
 
 
-declare var google: any;
-declare var map: any;
-
 
 @Component({
 	selector: 'view',
@@ -17,7 +14,8 @@ declare var map: any;
     background-color: yellow;
   }
   `
-	]
+	],
+	providers: [AuthenticationService, ArticlesService]
 })
 export class ViewComponent {
 
@@ -28,19 +26,14 @@ export class ViewComponent {
 	errorMessage: string;
 	allowEdit: boolean = false;
 	isClassVisible: any = false;
-	dateRange: any = {};
-	listDates: any;
-	imgToggleCount: number = 0;
-	map = null;
-	titleAlert:string ='';
-	order:any = {};
+	titleAlert: string = '';
 
 	constructor(
 		private _router: Router,
 		private _route: ActivatedRoute,
 		private _authenticationService: AuthenticationService,
 		private _articlesService: ArticlesService,
-		) { }
+	) { }
 
 
 	ngOnInit() {
@@ -53,93 +46,15 @@ export class ViewComponent {
 				article => {
 					this.article = article;
 					this.allowEdit = (this.user && this.user._id === this.article.creator._id);
-					this.initOrderDatesArray();
-					this.loadMap();
 				},
 				error => this._router.navigate(['/articles'])
 				);
 		});
-
-		this.order.phone== '';
-		this.order.content = '';
-	}
-
-	loadMap() {
-		setTimeout(() => {
-			this.map = new google.maps.Map(document.getElementById('assetMap'), {
-				center: { lat: this.article.location[1], lng: this.article.location[0] },
-				zoom: 12,
-				disableDefaultUI: true,
-				streetViewControl: false,
-				zoomControl: true,
-				draggable: false,
-				mapTypeControl: false,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			});
-		}, 0);
-	}
-
-	initOrderDatesArray() {
-
-	}
-
-	//toggle img index to view in gallery
-	toggleImg(next) {
-		if (next) {
-			if (this.article.imgUrl.length > (this.imgToggleCount + 1))
-				this.imgToggleCount++;
-			else
-				this.imgToggleCount = 0;
-		} else {
-			if ((this.imgToggleCount) == 0)
-				this.imgToggleCount = this.article.imgUrl.length - 1;
-			else
-				this.imgToggleCount--;
-		}
 	}
 
 	ngOnDestroy() {
 		this.routingObserver.unsubscribe();
 	}
 
-	delete() {
-		this._articlesService.delete(this.article._id).subscribe(deletedArticle => this._router.navigate(['/articles']),
-			error => this.errorMessage = error);
-	}
-
-
-	
-
-	sendOrderDateRequest() {
-		if(this.order.phone == '' || this.order.content == '')
-		this.titleAlert = 'יש למלא את כל השדות';
-	  
-		else{
-		 this.article.orderPhone = this.order.phone;
-		 this.article.orderContent = this.order.content;
-		this._articlesService.insertOrderDates(this.article).subscribe(order =>
-		 this.pushNotificatinToOwnAsset(order),
-		 error => this.titleAlert = error);
-		}
-	   }
-	   pushNotificatinToOwnAsset(order) {
-		var message = {
-		 text: 'bla bla pushNotificatinToOwnAsset',
-		 user: this.article.creator._id,
-		 articleId: this.article._id,
-		 orderId:order._id
-		};
-	  
-		// this._chatService.emit('NotificationOrderRequest', message);
-		this.titleAlert = 'הבקשה נשלחה בהצלחה';
-	   }
-
-	// changeColor(){
-	// 	setTimeout(() => {
-	// 		this.isClassVisible = !this.isClassVisible;
-	// 	}, 500);
-
-
-	// }	
 }
 
